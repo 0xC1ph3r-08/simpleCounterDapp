b00b738b8492d1e7ece40f1513beb2732acff742
0xc1ph3r-08 <0xc1ph3r.08@gmail.com>
2025-08-24 06:57:54 +0000

    edited constants.ts file

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,38 @@
-import {abi}  from "../foundry/out/Counter.sol/Counter.json"
-// /home/sunil-kumar/Desktop/counterDapp/foundry/out/Counter.sol/Counter.json
 
-export const CONTRACT_ABI= abi
+export const CONTRACT_ABI= [
+        {
+            "type": "function",
+            "name": "increment",
+            "inputs": [],
+            "outputs": [],
+            "stateMutability": "nonpayable"
+        },
+        {
+            "type": "function",
+            "name": "number",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "setNumber",
+            "inputs": [
+                {
+                    "name": "newNumber",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "nonpayable"
+        }
+    ]
 
 export const CONTRACT_ADDRESS="0x730029e6a64659cd19dc8a197eEfa57C66FB6778"
